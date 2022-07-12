7ecc945371c825a69d3107e8ec279e5989a6c2cb
limcid <limcidson@gmail.com>
2022-07-11 23:58:44 -0400

    Mousing over grid now draws a persistent line

--- etchasketch.js
+++ etchasketch.js
@@ -1,8 +1,15 @@
 const container = document.querySelector('#container');
 
-for (let i = 0; i < (16*16); i++) {
+for (let i = 0; i < (32*32); i++) {
     const grid_div = document.createElement('div');
     grid_div.classList.add('grid_element');
     // grid_div.textContent = i % 16;
     container.appendChild(grid_div);
+
+    grid_div.onmouseover = function() {
+        // alert("hello");
+        this.style.backgroundColor = 'green';
+    }
 }
+
+
