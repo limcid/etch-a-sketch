bc1d7f094a091a7309e4d6adcb58e2e9f7c49eb3
limcid <limcidson@gmail.com>
2022-07-12 20:03:38 -0400

    Added option to increase grid resolution. Exercise completed.

--- etchasketch.js
+++ etchasketch.js
@@ -1,15 +1,41 @@
-const container = document.querySelector('#container');
+const containerWidth = 640;
+var res;
 
-for (let i = 0; i < (32*32); i++) {
-    const grid_div = document.createElement('div');
-    grid_div.classList.add('grid_element');
-    // grid_div.textContent = i % 16;
-    container.appendChild(grid_div);
-
-    grid_div.onmouseover = function() {
-        // alert("hello");
-        this.style.backgroundColor = 'green';
+function drawCanvas() {
+    const container = document.querySelector('#container');
+    for (let i = 0; i < (res * res); i++) {
+        const grid_div = document.createElement('div');
+        grid_div.classList.add('grid_element');
+    
+        grid_div.style.width = (containerWidth / res) + 'px';
+        grid_div.style.height = grid_div.style.width;
+        
+        container.appendChild(grid_div);
+    
+        grid_div.onmouseover = function() {
+            this.style.backgroundColor = 'green';
+        }
     }
 }
 
+function changeGridRes() {
+    const clear_divs = document.getElementsByClassName('grid_element');
+        
+    const element = document.getElementById("container");
+ 	while (element.firstChild) {
+ 		element.removeChild(element.lastChild);
+  	}
 
+    let gridRes = prompt("How many squares per side (16 - 100)?");
+    res = parseInt(gridRes);
+
+    if (res < 16 || res > 100) {
+        alert("Please enter a value between 16 and 100. Using default value of 32.");
+        res = 32;
+    }
+
+    drawCanvas();
+}
+
+
+changeGridRes();
